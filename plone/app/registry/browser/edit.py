f7f4ef149ecc7ef17b8df4afe3b9b5812dfea748
Vincent Fretin <vincent.fretin@gmail.com>
2010-08-06 15:57:54 +0000

    Use plone domain to translate the five messages.
    svn path=/plone.app.registry/trunk/; revision=38575

--- plone/app/registry/browser/edit.py
+++ plone/app/registry/browser/edit.py
@@ -7,7 +7,7 @@ from z3c.form import form, field, button
 from Products.statusmessages.interfaces import IStatusMessage
 
 from zope.i18nmessageid import MessageFactory
-_ = MessageFactory('plone.app.registry')
+_ = MessageFactory('plone')
 
 class RecordEditForm(form.EditForm):
     """Edit a single record
@@ -31,19 +31,19 @@ class RecordEditForm(form.EditForm):
     def label(self):
         return _(u"Edit record: ${name}", mapping={'name': self.record.__name__})
 
-    @button.buttonAndHandler(_('Save'), name='save')
+    @button.buttonAndHandler(_(u"label_save", default=u"Save"), name='save')
     def handleSave(self, action):
         data, errors = self.extractData()
         if errors:
             self.status = self.formErrorsMessage
             return
         self.record.value = data['value']
-        IStatusMessage(self.request).addStatusMessage(_(u"Changes saved"), "info")
+        IStatusMessage(self.request).addStatusMessage(_(u"Changes saved."), "info")
         self.request.response.redirect(self.context.absolute_url())
 
-    @button.buttonAndHandler(_('Cancel'), name='cancel')
+    @button.buttonAndHandler(_(u"label_cancel", default=u"Cancel"), name='cancel')
     def handleCancel(self, action):
-        IStatusMessage(self.request).addStatusMessage(_(u"Edit cancelled"), "info")
+        IStatusMessage(self.request).addStatusMessage(_(u"Edit cancelled."), "info")
         self.request.response.redirect(self.context.absolute_url())
 
 class RecordEditView(layout.FormWrapper):
