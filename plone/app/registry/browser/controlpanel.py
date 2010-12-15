201892faad9fa90a86ef97413f3a3f2d1890e8d6
Rok Garbas <rok.garbas@gmail.com>
2010-12-15 11:29:29 +0000

    renaming prefix to schema_prefix because prefix is already used by z3c.form also adding .gitignore file and raising version to 1.0b3
    svn path=/plone.app.registry/trunk/; revision=46345

--- plone/app/registry/browser/controlpanel.py
+++ plone/app/registry/browser/controlpanel.py
@@ -39,10 +39,10 @@ class RegistryEditForm(AutoExtensibleForm, form.EditForm):
     """
 
     control_panel_view = "plone_control_panel"
-    prefix = None
+    schema_prefix = None
 
     def getContent(self):
-        return getUtility(IRegistry).forInterface(self.schema, prefix=self.prefix)
+        return getUtility(IRegistry).forInterface(self.schema, prefix=self.schema_prefix)
     
     def updateActions(self):
         super(RegistryEditForm, self).updateActions()
