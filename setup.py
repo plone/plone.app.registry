a2376ba63a680da0760ba76195d697171c12251d
WouterVH <WouterVH@localhost>
2011-04-08 22:26:05 +0000

    standardize metadate-files
    svn path=/plone.app.registry/trunk/; revision=48562

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='plone.app.registry',
       version=version,
       description="Zope 2 and Plone  integration for plone.registry",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open(os.path.join("CHANGES.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
