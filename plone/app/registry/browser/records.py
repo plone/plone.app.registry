6a3c8f0b07de8854fe2c81b7aff77ff933e41a3f
Nathan Van Gheem <vangheem@gmail.com>
2012-06-20 23:57:46 -0500

    add interface to search and page through registry settings

---
+++ plone/app/registry/browser/records.py
@@ -0,0 +1,42 @@
+from Products.Five import BrowserView
+from Products.CMFPlone.PloneBatch import Batch
+
+
+def _true(s, v):
+    return True
+
+
+def _is_in(s, v):
+    return s in v
+
+
+def _starts_with(s, v):
+    return v.startswith(s)
+
+
+class RecordsControlPanel(BrowserView):
+
+    def __call__(self):
+        form = self.request.form
+        search = form.get('q')
+        searchp = form.get('qp')
+        compare = _is_in
+        if searchp:
+            search = searchp
+        if search is not None and search.startswith('prefix:'):
+            search = search[len('prefix:'):]
+            compare = _starts_with
+        if not search:
+            compare = _true
+
+        self.prefixes = {}
+        self.records = []
+        for record in self.context.records.values():
+            ifaceNamePart = record.interfaceName.split('.')[-1]
+            if ifaceNamePart not in self.prefixes:
+                self.prefixes[ifaceNamePart] = record.interfaceName
+            if compare(search, record.__name__):
+                self.records.append(record)
+        self.records = Batch(self.records, 10,
+            int(form.get('b_start', '0')), orphan=1)
+        return self.index()
