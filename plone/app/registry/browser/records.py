90c8fcf1148c451622233c8c54aa1122bf66d53a
Nathan Van Gheem <vangheem@gmail.com>
2012-09-18 22:52:25 -0500

    handle control panel prefixes for fields that do not   have interfaces better.

--- plone/app/registry/browser/records.py
+++ plone/app/registry/browser/records.py
@@ -1,14 +1,24 @@
 from Products.Five import BrowserView
 from Products.CMFPlone.PloneBatch import Batch
 
+
 def _true(s, v):
     return True
+
 
 def _is_in(s, v):
     return s in v
 
+
 def _starts_with(s, v):
     return v.startswith(s)
+
+
+_okay_prefixes = [
+    'Products',
+    'plone.app',
+    'plone']
+
 
 class RecordsControlPanel(BrowserView):
 
@@ -29,11 +39,19 @@ class RecordsControlPanel(BrowserView):
         self.records = []
         for record in self.context.records.values():
             ifaceName = record.interfaceName
-            ifaceNamePart = ''
-            if not ifaceName is None:
-                ifaceNamePart = ifaceName.split('.')[-1]
-            if ifaceNamePart not in self.prefixes:
-                self.prefixes[ifaceNamePart] = record.interfaceName
+            if ifaceName is not None:
+                recordPrefix = ifaceName.split('.')[-1]
+                prefixValue = record.interfaceName
+            else:
+                for prefix in _okay_prefixes:
+                    name = record.__name__
+                    if name.startswith(prefix):
+                        recordPrefix = '.'.join(
+                            name.split('.')[:len(prefix.split('.')) + 1])
+                        prefixValue = recordPrefix
+                        break
+            if recordPrefix not in self.prefixes:
+                self.prefixes[recordPrefix] = prefixValue
             if compare(search, record.__name__):
                 self.records.append(record)
         self.records = Batch(self.records, 10,
