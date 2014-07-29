011d7d2a797cc7b6f45552cd33d123eadad1bea6
Gil Forcada <gforcada@gnome.org>
2014-07-30 01:12:10 +0200

    .txt -> .rst

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, find_packages
-import os
 
 version = '1.2.4.dev0'
 
 setup(name='plone.app.registry',
       version=version,
       description="Zope 2 and Plone  integration for plone.registry",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("CHANGES.txt")).read(),
+      long_description=open("README.rst").read() + "\n" +
+                       open("CHANGES.rst").read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
