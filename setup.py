30defd2be68c985bcdcd90bcc1f6c94dfbf71129
Maurits van Rees <maurits@vanrees.org>
2017-03-09 22:01:13 +0100

    Added classifiers to setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -7,10 +7,14 @@ setup(name='plone.app.registry',
       description="Zope 2 and Plone  integration for plone.registry",
       long_description=open("README.rst").read() + "\n" +
                        open("CHANGES.rst").read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from
+      # https://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='plone registry settings configuration',
