b44bcb4e7699a137345faede33811e7eb79cb366
Maurits van Rees <maurits@vanrees.org>
2021-08-04 18:38:30 +0200

    Add Python 3.9 classifier.
    [ci skip]

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords='plone registry settings configuration',
     author='Martin Aspeli',
