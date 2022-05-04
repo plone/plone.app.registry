2ed9526c8c36a83c0341a2cc47fcb8b6c09a68d1
Jens W. Klein <jk@kleinundpartner.at>
2022-05-04 21:54:30 +0200

    add newsfile

--- plone/app/registry/browser/records.py
+++ plone/app/registry/browser/records.py
@@ -2,7 +2,7 @@ from lxml.etree import XMLSyntaxError
 from plone.app.registry.exportimport.handler import RegistryExporter
 from plone.app.registry.exportimport.handler import RegistryImporter
 from plone.autoform.form import AutoExtensibleForm
-from plone.base import _
+from plone.base import PloneMessageFactory as _
 from plone.base.batch import Batch
 from plone.registry import field as registry_field
 from plone.registry import Record
